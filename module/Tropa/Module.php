fe7ff34d5ae004602cad9257d4187361c5d683b7
dinaerteneto <dinaerteneto@gmail.com>
2014-12-22 16:53:35 -0200

    LanternaForm

--- module/Tropa/Module.php
+++ module/Tropa/Module.php
@@ -37,6 +37,7 @@ class Module {
         );
         AbstractValidator::setDefaultTranslator($translator);
          */
+        $GLOBALS['sm'] = $e->getApplication()->getServiceManager();
     }
 
     public function getConfig() {
